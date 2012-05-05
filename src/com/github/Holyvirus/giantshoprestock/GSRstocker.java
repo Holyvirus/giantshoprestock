b59d629b7fa6571448b65135ea7795c6ed2454f2
Josh Aurora <alienartificial@hotmail.com>
2012-05-05 15:51:37 -0400

    .

--- src/com/github/Holyvirus/giantshoprestock/GSRstocker.java
+++ src/com/github/Holyvirus/giantshoprestock/GSRstocker.java
@@ -5,37 +5,38 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import nl.giantit.minecraft.GiantShop.API.stock.ItemNotFoundException;
-import nl.giantit.minecraft.GiantShop.API.stock.stockAPI;
+import nl.giantit.minecraft.GiantShop.API.GiantShopAPI;
 import nl.giantit.minecraft.GiantShop.API.stock.core.itemStock;
-import nl.giantit.minecraft.GiantShop.core.Items.Items;
 
 public class GSRstocker {
 
 	public static final Logger log = Logger.getLogger("Minecraft");
-	stockAPI stock;
 	itemStock ItemStock;
 	itemStock IStock;
-	Items items;
 	org.bukkit.configuration.ConfigurationSection CS;
 	GiantShopRestock GSR = GiantShopRestock.getGR();
 	
 	public void Restock(){
 		Set<String> keys = GSR.config.getConfigurationSection("Restock").getKeys(false);
 		if(keys != null) {
-			log.log(Level.WARNING, "You have no items that need to be restocked, please define what you want to restock using\"/restock [item] [amount]\"");
 			   for(String key : keys) {
 					try {
-						this.IStock = stock.getItemStock(key);
-						if(!GSR.config.getBoolean("AllowOverStock") && (IStock.getStock() + GSR.config.getInt("Restock." + key)) > IStock.getMaxStock()){
-							log.log(Level.INFO, "The restock of: " + key + " cancelled because you would overstock!");
+						int VID = GSR.GSItems.getItemIDByName(key).getId();
+							if(GSR.GSItems.isValidItem(VID)){
+							this.IStock = GiantShopAPI.Obtain().getStockAPI().getItemStock(key);
+								if(!GSR.config.getBoolean("AllowOverStock") && (IStock.getStock() + GSR.config.getInt("Restock." + key)) > IStock.getMaxStock()){
+									log.log(Level.INFO, "The restock of: " + key + " cancelled because you would overstock!");
+								}else{
+									IStock.setStock(IStock.getStock() + GSR.config.getInt("Restock." + key));
+							}
 						}else{
-							IStock.setStock(IStock.getStock() + GSR.config.getInt("Restock." + key));
+							log.log(Level.WARNING, "The config node:" + key + " is not valid!1");
 						}
 					} catch (ItemNotFoundException e) {
 						e.printStackTrace();
-						log.log(Level.WARNING, "the config node:" + key + "is not valid!");
+						log.log(Level.WARNING, "The item:" + key + " has not been set in the shop! Type \"/shop add [item] [amount] [buyfor]\"");
 					}
-			   }
+			   } 
 		}else{
 			log.log(Level.WARNING, "You have no items that need to be restocked, please define what you want to restock using\"/restock [item] [amount]\"");
 		}
--- src/com/github/Holyvirus/giantshoprestock/GiantShopRestock.java
+++ src/com/github/Holyvirus/giantshoprestock/GiantShopRestock.java
@@ -5,25 +5,20 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import nl.giantit.minecraft.GiantShop.GiantShop;
 import nl.giantit.minecraft.GiantShop.API.GiantShopAPI;
-import nl.giantit.minecraft.GiantShop.API.stock.core.itemStock;
 import nl.giantit.minecraft.GiantShop.core.Items.ItemID;
 import nl.giantit.minecraft.GiantShop.core.Items.Items;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scheduler.BukkitScheduler;
 
 public class GiantShopRestock extends JavaPlugin{
 	
@@ -31,9 +26,7 @@ public class GiantShopRestock extends JavaPlugin{
 	public static final Logger log = Logger.getLogger("Minecraft");
 	File configFile;
 	FileConfiguration config;
-	private BukkitScheduler BS;
-	private itemStock itemstock;
-	private GiantShopAPI API;
+	GiantShopAPI API;
 	public Items GSItems;
 	public ItemID enteredItem;
 	public static GiantShopRestock GR;
@@ -62,9 +55,7 @@ public class GiantShopRestock extends JavaPlugin{
 		this.GSRT = new GSRTime();
 		log.log(Level.INFO, "[GiantShopRestock] Was successfully enabled!");
 	}
-	
-	public GSRstocker GSRS;
-	public GSRTime GSRT;
+
 	public long lastTime = 0;
 	
 	@Override
@@ -109,16 +100,20 @@ public class GiantShopRestock extends JavaPlugin{
 	            e.printStackTrace();
 	        }
 	    }
+		
+		public GSRstocker GSRS;
+		public GSRTime GSRT;
+		
 	    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
 	    	if(cmd.getName().equalsIgnoreCase("restock") || cmd.getName().equalsIgnoreCase("rs")){
 	    		if (sender.hasPermission("giantshop.restock")){
 	    			if(args.length == 1){
 	    				if(args[0].equalsIgnoreCase("start")){
 	    	    			GSRT.doTask();
-	    	    			sender.sendMessage(ChatColor.GOLD + "The restock system has been started! Your next restock is in: " + "¤9" + GSRT.getTimeLeft());
+	    	    			sender.sendMessage(ChatColor.GOLD + "The restock system has been started! Your next restock is in: ¤9" + GSRT.getTimeLeft());
 	    				}else if(args[0].equalsIgnoreCase("stop")){
-	    					if(BS.isCurrentlyRunning(GSRT.doTaskID)){
-	    						BS.cancelTask(GSRT.doTaskID);
+	    					if(Bukkit.getServer().getScheduler().isCurrentlyRunning(GSRT.doTaskID)){
+	    						Bukkit.getServer().getScheduler().cancelTask(GSRT.doTaskID);
 	    						sender.sendMessage(ChatColor.RED + "The restock system has been stopped! All delays were reset!");
 	    					}else{
 	    		    			sender.sendMessage(ChatColor.RED + "The restock task is NOT running atm! Please type \"/rs start\" to start restocking!");
@@ -151,9 +146,10 @@ public class GiantShopRestock extends JavaPlugin{
 	    			sender.sendMessage(ChatColor.RED + "You do not have permission to use this command!");
 	    		}
 	    	}else if(cmd.getName().equalsIgnoreCase("restocktime") || cmd.getName().equalsIgnoreCase("rst")){
-	    		if(BS.isCurrentlyRunning(GSRT.doTaskID)){
+				log.log(Level.SEVERE, "the doTaskID is: " + GSRT.doTaskID);
+    					if(Bukkit.getServer().getScheduler().isCurrentlyRunning(GSRT.doTaskID)){
 	    			if(args.length < 1){
-	    				sender.sendMessage(ChatColor.GOLD + "The current time is: " + GSRT.humanNowTime + "the shop will next restock in: " + "¤9" + GSRT.getTimeLeft());
+	    				sender.sendMessage(ChatColor.GOLD + "The current time is: " + GSRT.humanNowTime + " the shop will next restock in: " + "¤9" + GSRT.getTimeLeft());
 	    			}else if(args.length == 1){
 	    					if (sender.hasPermission("giantshop.restock")){
 	    							config.set("RestockTime", args[0]);
@@ -164,6 +160,7 @@ public class GiantShopRestock extends JavaPlugin{
 	    					}
 	    			}
 	    		}else{
+					log.log(Level.SEVERE, "the doTaskID is: " + GSRT.doTaskID);
 	    			sender.sendMessage(ChatColor.GOLD + "The restock task is NOT running atm! Please type \"/rs start\" to start restocking!");
 	    		}
 	    	}
@@ -172,17 +169,16 @@ public class GiantShopRestock extends JavaPlugin{
 
 	    public void write(CommandSender sender, String Item, int FAmt){
 	    	ItemID FItem = GSItems.getItemIDByName(Item);
-	    	if(GSItems.isValidItem(FItem.getId())){
-	    		getConfig().set("Restock." + Item, FAmt);
-	    		try {
-					getConfig().save(configFile);
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-	    	}else{
-	    		sender.sendMessage("You have not specified a valid item!");
+	    	if(FItem != null){
+		    		getConfig().set("Restock." + Item, FAmt);
+		    		sender.sendMessage("You have set the item " + Item + " to be restocked by " + FAmt + " each restock!");
+		    		try {
+						getConfig().save(configFile);
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+	    		}
 	    	}
-	    }
 	    
 	    public void write(CommandSender sender, int FItem, int FType, int FAmt){
 	    	if(GSItems.isValidItem(FItem, FType)){
@@ -195,5 +191,5 @@ public class GiantShopRestock extends JavaPlugin{
 	    	}else{
 	    		sender.sendMessage("You have not specified a valid item!");
 	    	}
-	    }
+	  }
 }
